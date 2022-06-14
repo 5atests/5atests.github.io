6350770ccc77854b9e94180d121b9735c049c02a
5atests <danilovesmilk.milk@gmail.com>
2022-06-14 16:06:15 +0800

    uwu i wanna die

--- tests/public.js
+++ tests/public.js
@@ -5,13 +5,13 @@ const questions = document.querySelectorAll('span');
 console.debug(questions)
 
 for (let i in questions) {
-    console.debug('Setting up question...')
+    console.debug('Setting up question...', typeof i)
     let clone = structuredClone(i);
     clone.innerHTML = `
 <h3>${i.innerText}</h3><br>
 <input id="${i.id}" type="text">
 <br>
     `
-    form.replaceChild(clone, i)
+    form.replaceWith(clone)
     console.debug('Successfully updated element: ', i)
 }
